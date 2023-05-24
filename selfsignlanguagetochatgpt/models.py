25b5cd94bb08d7b793fac87754baf0757c8e6b25
egoing <egoing@gmail.com>
2023-05-24 11:04:02 +0900

    first commit

---
+++ selfsignlanguagetochatgpt/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Result(models.Model):
+    image = models.ImageField(blank=True)
+    answer = models.CharField(max_length=10,null=True)
+    result = models.CharField(max_length=10)
+    is_correct= models.BooleanField(default=False)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return f"result : {self.result}, answer : {self.answer}"
+    
+class ChatResult(models.Model):
+    prompt = models.CharField(max_length=1000)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.prompt
+    
